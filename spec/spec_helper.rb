dbf68d231f552b1aa5692762fd42645183d4d0d2
eityans <eityans@gmail.com>
2022-03-13 16:58:05 +0900

    set path

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,10 @@ SimpleCov.start 'rails'
 
 require 'simplecov'
 require 'simplecov-lcov'
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start
 
